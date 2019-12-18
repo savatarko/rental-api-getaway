31f972987320445a503613b00586219ed6a9957b
com <mihailo.radovic@gamecredits.com>
2019-12-18 08:03:35 +0100

    initial commit

---
+++ src/main/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplication.java
@@ -0,0 +1,14 @@
+package com.raf.cinemaapigetaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class CinemaApiGetawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApiGetawayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.cinemaapigetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApiGetawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
