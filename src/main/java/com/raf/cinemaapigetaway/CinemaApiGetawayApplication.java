57553b7ef561aee6441d19d492c0dc1a236d0912
Mihailo Radovic <mihailo.radovic@iconsolutions.com>
2019-12-18 09:58:07 +0100

    a

--- src/main/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplication.java
+++ src/main/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplication.java
@@ -2,6 +2,7 @@ package com.raf.cinemaapigetaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
