176719e0681173b957a007be4680d766e729e410
com <mihailo.radovic@gamecredits.com>
2019-12-31 17:08:32 +0100

    Add cors configuration

--- src/main/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplication.java
+++ src/main/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplication.java
@@ -2,11 +2,30 @@ package com.raf.cinemaapigetaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class CinemaApiGetawayApplication {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaApiGetawayApplication.class, args);
