57d385276344d6bd6ed2cb6c15d14a26259aa7df
savatarko <ivkovic3@gmail.com>
2023-01-18 20:38:45 +0100

    fix

--- src/main/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplication.java
+++ src/main/java/com/raf/cinemaapigetaway/CinemaApiGetawayApplication.java
@@ -3,6 +3,7 @@ package com.raf.cinemaapigetaway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class CinemaApiGetawayApplication {
 
     @Bean
